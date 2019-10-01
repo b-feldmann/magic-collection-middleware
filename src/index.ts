24db8ee7c697ce363472bfc94c86bc40265a314e
Benjamin Feldmann <benjamin-feldmann@web.de>
2019-10-01 15:15:22 +0200

    update build

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,9 @@ if (require.main === module) {
       app.get('*', (req, res) =>{
         res.sendFile(path.join(`${__dirname}/public/index.html`));
       });
+      app.get('/*', (req, res) =>{
+        res.sendFile(path.join(`${__dirname}/public/index.html`));
+      });
     }
   );
 }
